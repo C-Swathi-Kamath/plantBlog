e1b975307ba7e63bb3266da47ac0dcd9128dd396
Swathi Kamath <cswathikamath@gmail.com>
2022-08-19 00:27:22 +0530

    include footer with php

--- gallery.php
+++ gallery.php
@@ -61,16 +61,7 @@
 
   <!-- Footer -->
 
-  <footer id="footer">
-    <div class="container-fluid">
-      <a href="https://www.facebook.com/" class="link-dark"><i class="social-icon fab fa-facebook-f"></i></a>
-      <a href="https://twitter.com/" class="link-dark"><i class="social-icon fab fa-twitter"></i></a>
-      <a href="https://www.instagram.com/?hl=en" class="link-dark"><i class="social-icon fab fa-instagram"></i></a>
-      <a href="https://mail.google.com/" class="link-dark"><i class="social-icon fas fa-envelope"></i></a>
-      <p class="footer-para">Made with 💚 in Nitte</p>
-      <p>© Copyright 2022 Rooted</p>
-    </div>
-  </footer>
+  <?php include_once('includes/footer.php'); ?>
 
 
 
--- signup.php
+++ signup.php
@@ -76,18 +76,8 @@
   </section>
 
 
-  <!-- Footer -->
-
-  <footer id="footer">
-    <div class="container-fluid">
-      <a href="https://www.facebook.com/" class="link-dark"><i class="social-icon fab fa-facebook-f"></i></a>
-      <a href="https://twitter.com/" class="link-dark"><i class="social-icon fab fa-twitter"></i></a>
-      <a href="https://www.instagram.com/?hl=en" class="link-dark"><i class="social-icon fab fa-instagram"></i></a>
-      <a href="https://mail.google.com/" class="link-dark"><i class="social-icon fas fa-envelope"></i></a>
-      <p class="footer-para">Made with 💚 in Nitte</p>
-      <p>© Copyright 2022 Rooted</p>
-    </div>
-  </footer>
+ <!-- Footer -->
+ <?php include_once('includes/footer.php'); ?>
 
 
 
